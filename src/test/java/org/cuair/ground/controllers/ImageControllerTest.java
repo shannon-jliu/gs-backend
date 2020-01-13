5304a9db958b97ce4928d786a1dff80e956fff85
Luca Koval <lucakoval@me.com>
2020-01-13 13:18:52 -0500

    Testing post

--- src/test/java/org/cuair/ground/controllers/ImageControllerTest.java
+++ src/test/java/org/cuair/ground/controllers/ImageControllerTest.java
@@ -13,11 +13,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.mock.web.MockMultipartFile;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.io.*;
 import org.cuair.ground.models.Image;
 
 @RunWith(SpringRunner.class)
@@ -64,5 +66,25 @@ public class ImageControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/image/0").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isNoContent())
                 .andExpect(content().string(equalTo("")));
+    }
+
+    @Test
+    public void getFile() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/image/file/1.jpeg").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent())
+                .andExpect(content().string(equalTo("")));
+    }
+
+    @Test
+    public void create() throws Exception {
+        InputStream is = new BufferedInputStream(new FileInputStream("src/test/java/org/cuair/ground/controllers/test_images/test_0.jpg"));
+        MockMultipartFile firstFile = new MockMultipartFile("data", "test_0.jpg", "image", is);
+
+        mvc.perform(MockMvcRequestBuilders.multipart("/image")
+                .file(firstFile)
+                .param("jsonString", "{\"timestamp\":10006,\"imgMode\":\"retract\"}")
+            )
+                .andExpect(status().isOk());
+                // .andExpect(content().string(equalTo(list.toString())));
     }
 }
