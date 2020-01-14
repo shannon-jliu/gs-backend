0f3af43afb216739eef81e571a186fa2e4c3cf95
mariasam1 <ms2878@cornell.edu>
2020-01-14 11:46:48 -0500

    Adding flags (#15)
    * init
    
    * some more init
    
    * adding flags
    
    * docs

--- src/main/org/cuair/ground/controllers/TestController.java
+++ src/main/org/cuair/ground/controllers/TestController.java
@@ -2,12 +2,17 @@ package org.cuair.ground.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.cuair.ground.util.Flags;
 
 @RestController
 public class TestController {
 
+	// Unused, temporarily for display purposes
+	private String CUAIR_PLANE_CAMERA_GIMBAL = Flags.CUAIR_PLANE_CAMERA_GIMBAL;
+
     @RequestMapping(value = "/")
     public String index() {
+
       return "test";
     }
 
