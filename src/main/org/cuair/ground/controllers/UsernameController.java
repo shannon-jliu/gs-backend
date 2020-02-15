0f3b772c04ab981f0244dedc5aa847a6d0979fcb
Eric Feng <ef23@users.noreply.github.com>
2020-02-15 15:12:53 -0500

    Changing name to Username (#32)
    * Changing name to Username
    
    * Changing documentation

--- src/main/org/cuair/ground/controllers/UsernameController.java
+++ src/main/org/cuair/ground/controllers/UsernameController.java
@@ -2,11 +2,13 @@ package org.cuair.ground.controllers;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+import javax.servlet.http.HttpServletRequest;
 import org.cuair.ground.daos.DAOFactory;
 import org.cuair.ground.daos.DatabaseAccessor;
-import org.cuair.ground.models.User;
+import org.cuair.ground.models.Username;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 /** Contains all the callbacks for all the public api endpoints for the User  */
 @CrossOrigin
 @RestController
-@RequestMapping(value = "/user")
-public class UserController {
-  private DatabaseAccessor userDao = (DatabaseAccessor) DAOFactory.getDAO(DAOFactory.ModelDAOType.DATABASE_ACCESSOR, User.class);
+@RequestMapping(value = "/username")
+public class UsernameController {
+  private DatabaseAccessor usernameDao = (DatabaseAccessor) DAOFactory.getDAO(DAOFactory.ModelDAOType.DATABASE_ACCESSOR, Username.class);
 
   @RequestMapping(value = "/create", method = RequestMethod.POST)
-  public ResponseEntity create(String username) {
+  public ResponseEntity create(@RequestBody String username, HttpServletRequest request) {
+    String address = request.getRemoteAddr();
     return ok(username);
   }
 }
