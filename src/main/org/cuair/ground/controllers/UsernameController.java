30b0c0e19b6acf325bcbdd717b7ea75f67cb7d1c
Eric Feng <ef23@users.noreply.github.com>
2020-02-18 18:47:22 -0500

    Basic Username Controller (#36)
    * Adding in Username DAO and logic for usernames
    
    * Adding tests
    
    * Remove print statement
    
    * Formatting and documentation
    
    * Fixing documentation
    
    * Copy pasta from assignmentController PR to add usernameDAO as a modelless DAO
    
    * Fixing username DAO declaration

--- src/main/org/cuair/ground/controllers/UsernameController.java
+++ src/main/org/cuair/ground/controllers/UsernameController.java
@@ -1,10 +1,11 @@
 package org.cuair.ground.controllers;
 
+import static org.springframework.http.ResponseEntity.badRequest;
 import static org.springframework.http.ResponseEntity.ok;
 
 import javax.servlet.http.HttpServletRequest;
 import org.cuair.ground.daos.DAOFactory;
-import org.cuair.ground.daos.DatabaseAccessor;
+import org.cuair.ground.daos.UsernameDatabaseAccessor;
 import org.cuair.ground.models.Username;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,11 +19,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/username")
 public class UsernameController {
-  private DatabaseAccessor usernameDao = (DatabaseAccessor) DAOFactory.getDAO(DAOFactory.ModelDAOType.DATABASE_ACCESSOR, Username.class);
+  private UsernameDatabaseAccessor usernameDao = (UsernameDatabaseAccessor) DAOFactory.getDAO(DAOFactory.ModellessDAOType.USERNAME_DATABASE_ACCESSOR);
 
+  /**
+   *
+   * @param username - the username to create, sent as a raw object
+   * @param request - the request object that will contain the remoteAddr of this request
+   * @return 200 if the username is created successfully, otherwise 400 with a descriptive error message
+   */
   @RequestMapping(value = "/create", method = RequestMethod.POST)
   public ResponseEntity create(@RequestBody String username, HttpServletRequest request) {
     String address = request.getRemoteAddr();
+    String otherAddr = usernameDao.findAddrForUsername(username);
+
+    if (otherAddr != null) {
+      if (!otherAddr.equals(address)) {
+        return badRequest().body("Username already exists: " + username);
+      } else {
+        // if it reaches here, then the username, address matches and the user is re-logging in
+        return ok(username);
+      }
+    }
+
+    String otherName = usernameDao.findUserForAddress(address);
+    if (otherName != null) {
+      return badRequest().body("Address already exists with username: " + otherName);
+    }
+
+    usernameDao.create(new Username(username, address));
     return ok(username);
   }
 }
