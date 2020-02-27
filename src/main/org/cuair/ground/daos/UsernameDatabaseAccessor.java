88c5cca6d93c62a62a14918acf93c8e0b6bdfee2
max-charlamb <44248479+max-charlamb@users.noreply.github.com>
2020-02-26 23:06:24 -0500

    Added Functionality to Username DAO  (#42)
    * added functions in username dao to get username object from username string or address string
    
    * fixed request

--- src/main/org/cuair/ground/daos/UsernameDatabaseAccessor.java
+++ src/main/org/cuair/ground/daos/UsernameDatabaseAccessor.java
@@ -3,9 +3,7 @@ package org.cuair.ground.daos;
 import io.ebean.Ebean;
 import org.cuair.ground.models.Username;
 
-/**
- * Database Accessor object for Username model
- */
+/** Database Accessor object for Username model */
 public class UsernameDatabaseAccessor extends DatabaseAccessor<Username> {
   UsernameDatabaseAccessor() {
     super(Username.class);
@@ -13,6 +11,7 @@ public class UsernameDatabaseAccessor extends DatabaseAccessor<Username> {
 
   /**
    * Find out if the username already exists in the database
+   *
    * @param username the username to find
    * @return the address this username is associated with, or null if the username does not exist
    */
@@ -26,6 +25,7 @@ public class UsernameDatabaseAccessor extends DatabaseAccessor<Username> {
 
   /**
    * Finds if there is a username associated with this address
+   *
    * @param address - the address to search the database for
    * @return the username associated with this address, null if this address does not exist
    */
@@ -35,5 +35,25 @@ public class UsernameDatabaseAccessor extends DatabaseAccessor<Username> {
       return userObj.getUsername();
     }
     return null;
+  }
+
+  /**
+   * Finds the username object associated with username in the database
+   *
+   * @param username the username string to find
+   * @return the username object associated with username, if it doesn't exist then null.
+   */
+  public Username getUsernameFromName(String username) {
+    return Ebean.find(Username.class).where().eq("username", username).findOne();
+  }
+
+  /**
+   * Finds the username object associated with address in the database
+   *
+   * @param address the address string to find
+   * @return the username object associated with address, if it doesn't exist then null.
+   */
+  public Username getUsernameFromAddress(String address) {
+    return Ebean.find(Username.class).where().eq("address", address).findOne();
   }
 }
