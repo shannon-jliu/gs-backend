a50433528973cd2475e232571b0c25264c3a66f2
Luca Koval <lucakoval@me.com>
2021-02-27 15:45:06 -0500

    Testing

--- src/main/org/cuair/ground/controllers/StreamController.java
+++ src/main/org/cuair/ground/controllers/StreamController.java
@@ -39,6 +39,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.lang.InterruptedException;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import io.humble.video.Codec;
+import io.humble.video.Encoder;
+import io.humble.video.MediaAudio;
+import io.humble.video.MediaPacket;
+import io.humble.video.MediaPicture;
+import io.humble.video.Muxer;
+import io.humble.video.MuxerFormat;
+import io.humble.video.PixelFormat;
+import io.humble.video.Rational;
+import io.humble.video.awt.MediaPictureConverter;
+import io.humble.video.awt.MediaPictureConverterFactory;
 
 /** Contains all the callbacks for all the public api endpoints for the Image */
 @CrossOrigin
@@ -47,6 +64,104 @@ import org.springframework.web.multipart.MultipartFile;
 public class StreamController {
   /** String path to the folder where all the images are stored */
   private String streamImageDir = Flags.STREAM_IMAGE_DIR;
+
+  private Integer extension = 0;
+  private Integer numImg = 0;
+  private Muxer muxer;
+  private Encoder encoder;
+  private MediaPacket packet;
+  private MediaPicture picture;
+  private PixelFormat.Type pixelformat;
+  private Rational framerate;
+  private MediaPictureConverter converter;
+
+  private void setupMuxer() throws InterruptedException, IOException {
+  	/** First we create a muxer using the passed in filename and formatname if given. */
+		muxer = Muxer.make("testVideo_" + extension + ".mp4", null, "mp4");
+
+		/** Now, we need to decide what type of codec to use to encode video. Muxers
+     * have limited sets of codecs they can use. We're going to pick the first one that
+     * works, or if the user supplied a codec name, we're going to force-fit that
+     * in instead.
+     */
+    final MuxerFormat format = muxer.getFormat();
+    final Codec codec = Codec.findEncodingCodecByName("mpeg4");
+    framerate = Rational.make(1, 24);
+
+    /**
+     * Now that we know what codec, we need to create an encoder
+     */
+    encoder = Encoder.make(codec);
+
+    /**
+     * Video encoders need to know at a minimum:
+     *   width
+     *   height
+     *   pixel format
+     * Some also need to know frame-rate (older codecs that had a fixed rate at which video files could
+     * be written needed this). There are many other options you can set on an encoder, but we're
+     * going to keep it simpler here.
+     */
+    encoder.setWidth(1280);
+    encoder.setHeight(720);
+    // We are going to use 420P as the format because that's what most video formats these days use
+		pixelformat = PixelFormat.Type.PIX_FMT_YUV420P;
+    encoder.setPixelFormat(pixelformat);
+    encoder.setTimeBase(framerate);
+
+    /** An annoynace of some formats is that they need global (rather than per-stream) headers,
+     * and in that case you have to tell the encoder. And since Encoders are decoupled from
+     * Muxers, there is no easy way to know this beyond
+     */
+    if (format.getFlag(MuxerFormat.Flag.GLOBAL_HEADER))
+      encoder.setFlag(Encoder.Flag.FLAG_GLOBAL_HEADER, true);
+
+    /** Open the encoder. */
+    encoder.open(null, null);
+
+    /** Add this stream to the muxer. */
+    muxer.addNewStream(encoder);
+
+    /** And open the muxer for business. */
+    muxer.open(null, null);
+
+    /** Next, we need to make sure we have the right MediaPicture format objects
+     * to encode data with. Java (and most on-screen graphics programs) use some
+     * variant of Red-Green-Blue image encoding (a.k.a. RGB or BGR). Most video
+     * codecs use some variant of YCrCb formatting. So we're going to have to
+     * convert. To do that, we'll introduce a MediaPictureConverter object later. object.
+     */
+    MediaPictureConverter converter = null;
+    picture = MediaPicture.make(
+        encoder.getWidth(),
+        encoder.getHeight(),
+        pixelformat);
+    picture.setTimeBase(framerate);
+
+    packet = MediaPacket.make();
+  }
+
+  @PostConstruct
+	public void initialize() throws InterruptedException, IOException {
+		setupMuxer();
+	}
+
+	@PreDestroy
+  public void preDestroy() {
+  	/** Encoders, like decoders, sometimes cache pictures so it can do the right key-frame optimizations.
+     * So, they need to be flushed as well. As with the decoders, the convention is to pass in a null
+     * input until the output is not complete.
+     */
+  	System.out.println("YERRRD");
+    do {
+      encoder.encode(packet, null);
+      if (packet.isComplete())
+        muxer.write(packet, false);
+    } while (packet.isComplete());
+
+    /** Finally, let's clean up after ourselves. */
+    muxer.close();
+  }
 
   /**
    *
@@ -72,10 +187,47 @@ public class StreamController {
   }
 
   /**
+   * Convert a {@link BufferedImage} of any type, to {@link BufferedImage} of a
+   * specified type. If the source image is the same type as the target type,
+   * then original image is returned, otherwise new image of the correct type is
+   * created and the content of the source image is copied into the new image.
+   *
+   * @param sourceImage
+   *          the image to be converted
+   * @param targetType
+   *          the desired BufferedImage type
+   *
+   * @return a BufferedImage of the specifed target type.
+   *
+   * @see BufferedImage
+   */
+  public static BufferedImage convertToType(BufferedImage sourceImage, int targetType) {
+    BufferedImage image;
+
+    // if the source image is already the target type, return the source image
+
+    if (sourceImage.getType() == targetType) {
+      image = sourceImage;
+    }
+
+    // otherwise create a new image of the target type and draw the new
+    // image
+
+    else
+    {
+      image = new BufferedImage(sourceImage.getWidth(),
+          sourceImage.getHeight(), targetType);
+      image.getGraphics().drawImage(sourceImage, 0, 0, null);
+    }
+
+    return image;
+  }
+
+  /**
    *
    */
   @RequestMapping(method = RequestMethod.POST)
-  public ResponseEntity incoming(@RequestPart("file") MultipartFile file) {
+  public ResponseEntity incoming(@RequestPart("file") MultipartFile file) throws InterruptedException, IOException {
     File imageFile;
     try {
       imageFile = getImageFile(file);
@@ -85,9 +237,9 @@ public class StreamController {
     }
 
     // store the image locally
+    File newImageFile = FileUtils.getFile(streamImageDir + System.currentTimeMillis() + ".png");
     try {
-      FileUtils.moveFile(imageFile,
-          FileUtils.getFile(streamImageDir + System.currentTimeMillis() + ".png"));
+      FileUtils.moveFile(imageFile, newImageFile);
     } catch (FileExistsException e) {
       return badRequest().body("File with timestamp already exists");
     } catch (IOException e) {
@@ -98,7 +250,46 @@ public class StreamController {
 
     // LOGIC FOR STARTING NEW MUX/VIDEO
 
+    if ((numImg != 0) && (numImg % 24 == 0)) {
+    	/** Encoders, like decoders, sometimes cache pictures so it can do the right key-frame optimizations.
+	     * So, they need to be flushed as well. As with the decoders, the convention is to pass in a null
+	     * input until the output is not complete.
+	     */
+	  	System.out.println("YERRRD");
+	    do {
+	      encoder.encode(packet, null);
+	      if (packet.isComplete())
+	        muxer.write(packet, false);
+	    } while (packet.isComplete());
 
+	    /** Finally, let's clean up after ourselves. */
+	    muxer.close();
+
+			/** Restart but with a new video segment */
+			extension++;
+			setupMuxer();
+    }
+
+    // Begin muxing
+    BufferedImage image = ImageIO.read(newImageFile);
+    final BufferedImage screen = convertToType(image, BufferedImage.TYPE_3BYTE_BGR);
+
+    converter = MediaPictureConverterFactory.createConverter(screen, picture);
+    converter.toPicture(picture, screen, System.currentTimeMillis());
+
+    /** This is LIKELY not in YUV420P format, so we're going to convert it using some handy utilities. */
+    // if (converter == null)
+      // converter = MediaPictureConverterFactory.createConverter(screen, picture);
+    // converter.toPicture(picture, screen, i);
+
+    do {
+      encoder.encode(packet, picture);
+      if (packet.isComplete())
+        muxer.write(packet, false);
+    } while (packet.isComplete());
+
+    System.out.println("muxed " + numImg);
+    numImg++;
     return ok("We have received and saved the image");
   }
 }
