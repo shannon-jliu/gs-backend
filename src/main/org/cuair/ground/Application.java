9993f250bdcce4873a6a3a3795c51faa323b73de
Jack Williamson <jew256@cornell.edu>
2022-02-12 17:40:13 -0500

    Deleted unnecessary imports in

--- src/main/org/cuair/ground/Application.java
+++ src/main/org/cuair/ground/Application.java
@@ -3,14 +3,6 @@ package org.cuair.ground;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.cuair.ground.util.Flags;
-import org.freedesktop.gstreamer.Gst;
-import org.freedesktop.gstreamer.Pipeline;
-import org.freedesktop.gstreamer.Version;
-
-import java.io.File;
-import java.util.stream.Stream;
-
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
