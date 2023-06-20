b35323c8071cde73470dc0a0b3a67605fa380938
Owen Sorber <ons3@cornell.edu>
2023-06-19 21:37:19 -0400

    force drop db

--- drop_and_create_db.sh
+++ drop_and_create_db.sh
@@ -1,2 +1,2 @@
-psql -U postgres -c "DROP DATABASE groundserver"
+psql -U postgres -c "DROP DATABASE groundserver  with (FORCE)"
 psql -U postgres -c "CREATE DATABASE groundserver WITH ENCODING='UTF8' OWNER=postgres CONNECTION LIMIT=-1;"
